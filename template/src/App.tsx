01b71e0cb57eac57d244a4b2231d3eff6a2b16b6
Owen Evans <owen@3sidedcube.com>
2022-01-07 11:05:35 +0000

    sass compiler fixes, README updated

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,15 +1,15 @@
 import logo from "./logo.svg";
-import "./App.scss";
+import styles from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles["app"]}>
+      <header className={styles["app-header"]}>
+        <img src={logo} className={styles["app-logo"]} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a className={styles["app-link"]} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
